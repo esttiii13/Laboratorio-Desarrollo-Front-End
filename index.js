186dff45af0210b34e77d583626fd6fcfcd04bef
esttiii13 <125816301+esttiii13@users.noreply.github.com>
2023-02-19 20:45:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+var myvar = '<html>'+
+'	<head>'+
+'		<title> Create account </title>'+
+'		<style>'+
+'		</style>'+
+'	</head>'+
+'	<body>'+
+'		<form>'+
+'			<ul>'+
+'				<li>'+
+'					<label for="name">Nombre:</label>'+
+'					<input type="text" name="nombre" value="" required /><br/>'+
+'				</li>'+
+'				<li>	'+
+'					<label for="mail">Email:</label>'+
+'					<input type="email" name="e-correo" value="" required /><br/>'+
+'				</li>'+
+'				<li>'+
+'					<label for="password">Clave:</label>'+
+'					<input type="password" name="clave1" minlength="8"/ required><br/>'+
+'				</li>'+
+'				<li>'+
+'				<label for="password">Confirme su clave:</label>'+
+'				<input type="password" name="clave2" minlength="8"/ required><br/>'+
+'<script>'+
+'if (clave1 != clave2) {'+
+'	window.alert (\'Las contraseÃ±as no coinciden\');'+
+'} else {'+
+'</script>'+
+'				</li>'+
+'			</ul>'+
+'			<button type="submit">Enviar</button>'+
+'				<script window.alert("La inscripciÃ³n ha sido correcta")></script>'+
+'		</form>'+
+'	</body>'+
+'</html>';
+	
